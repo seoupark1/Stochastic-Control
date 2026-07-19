7f4c2638c0f399076b50f3768654a90209c63b0a
seoupark1 <seoupark05@gmail.com>
2026-07-19 11:57:43 +0000

    작은 오류 수정 및 non-additive noise에 대해서도 적용 가능하도록 변경

--- src/stochastic_control/estimation/extended_kalman_filter.py
+++ src/stochastic_control/estimation/extended_kalman_filter.py
@@ -2,7 +2,6 @@ import numpy as np
 from numpy.typing import ArrayLike
 from collections.abc import Callable
 
-# only for additive noise
 class ExtendedKalmanFilter:
     
     def __init__(self,
@@ -10,8 +9,10 @@ class ExtendedKalmanFilter:
                 covariance: ArrayLike,
                 motion_model: Callable,
                 motion_jacobian: Callable,
+                motion_noise_jacobian: ArrayLike,
                 measurement_model: Callable,
-                measurement_jacobian: ArrayLike,
+                measurement_jacobian: Callable,
+                measurement_noise_jacobian: ArrayLike,
                 motion_noise_covariance: ArrayLike,
                 measurement_noise_covariance: ArrayLike):
         
@@ -22,10 +23,12 @@ class ExtendedKalmanFilter:
         # motion model & jacobian
         self.f_model = motion_model
         self.F_jacobian = motion_jacobian
+        self.L_jacobian = np.asarray(motion_noise_jacobian, dtype = float)
 
         # measurement model & jacobian
         self.h_model = measurement_model
         self.H_jacobian = measurement_jacobian
+        self.M_jacobian = np.asarray(measurement_noise_jacobian, dtype = float)
 
         # noise covariances
         self.Q = np.asarray(motion_noise_covariance, dtype = float)
@@ -40,25 +43,27 @@ class ExtendedKalmanFilter:
         F = np.asarray(self.F_jacobian(self.x, u, 0), dtype = float)
 
         # x_check, P_check about k step
-        self.x = np.f_model(self.x, u, 0).reshape(-1)
-        self.P = F @ self.P @ F.T + self.Q
+        self.x = self.f_model(self.x, u, 0).reshape(-1)
+        self.P = F @ self.P @ F.T + self.L_jacobian @ self.Q @ self.L_jacobian.T
+        self.P = (self.P + self.P.T) / 2
 
     def correction(self,
                    measurement_vector: ArrayLike):
         
         y = np.asarray(measurement_vector, dtype = float).reshape(-1)
-        measurement_size = self.h_jacobian.shape[0]
+        measurement_size = self.x.size
 
-        # H jacobian about k-1 step
+        # H jacobian @ h about k step
         H = np.asarray(self.H_jacobian(self.x, 0), dtype = float)
+        h = np.asarray(self.h_model(self.x, 0), dtype = float).reshape(-1)
 
         # kalman gain
-        K = self.P @ H.T @ np.linalg.inv(H @ self.P @ H.T + self.R)
+        K = self.P @ H.T @ np.linalg.inv(H @ self.P @ H.T + self.M_jacobian @ self.R @ self.M_jacobian.T)
         
-        # x_hat, P_hat about K step
-        x_check, P_check = self.x, self.P
-        self.x = x_check + K @ (y - self.h_model(x_check, 0))
-        self.P = (np.eye(measurement_size) - K @ H) @ P_check
+        # x_hat, P_hat about k step
+        self.x = self.x + K @ (y - h)
+        self.P = (np.eye(measurement_size) - K @ H) @ self.P
+        self.P = (self.P + self.P.T) / 2
 
     def extendedkalmanfilter(self,
                              control_vector: ArrayLike,
