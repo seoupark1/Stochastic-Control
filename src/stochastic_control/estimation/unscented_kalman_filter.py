7dfe3a498821a38d57bedc9813981f1d4dc59b04
seoupark1 <seoupark05@gmail.com>
2026-07-28 13:57:36 +0000

    prediction을 거친 covariance를 사용해 correction 단계의 sigma points를 구함

--- src/stochastic_control/estimation/unscented_kalman_filter.py
+++ src/stochastic_control/estimation/unscented_kalman_filter.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.typing import ArrayLike
+from numpy.typing import ArrayLike, NDArray
 from collections.abc import Callable
 
 class UnscentedKalmanFilter:
@@ -24,61 +24,52 @@ class UnscentedKalmanFilter:
         self.h_model = measurement_model
         self.R = np.asarray(measurement_noise_covariance, dtype = float)
 
-        # useful parameters
-        self.propagated_sigma_points = None
+    def get_sigma_points(self,
+                         mean: ArrayLike,
+                         covariance: ArrayLike) -> NDArray[np.float64]:
 
-    def get_propagated_sigma_points(self,
-                                    control_vector: ArrayLike):
-
-        # control vector u
-        u = np.asarray(control_vector, dtype = float).reshape(-1)
-        n = self.mean.shape[0]
+        n = mean.shape[0]
         kappa = 3 - n
 
         # cholesky decomposition
-        L = np.linalg.cholesky(self.P)
+        L = np.linalg.cholesky(covariance)
 
         # compute sigma points
-        sigma_points = np.vstack((self.mean,
-                                  self.mean + np.sqrt(n + kappa) * L.T,
-                                  self.mean - np.sqrt(n + kappa) * L.T))
-        
-        # propagate sigma points
-        propagated_sigma_points = np.zeros_like(sigma_points)
-        for i in range(2 * n + 1):
-            propagated_sigma_points[i, :] = self.f_model(sigma_points[i, :], u, 0)        
-        
-        return propagated_sigma_points
+        sigma_points = np.vstack((mean,
+                                  mean + np.sqrt(n + kappa) * L.T,
+                                  mean - np.sqrt(n + kappa) * L.T))
+
+        return sigma_points
     
     def get_weights(self):
         n = self.mean.shape[0]
         kappa = 3 - n
 
         # weights
-        a = np.full(2 * n + 1, 1 / (2 * (n + kappa)))
-        a[0] = kappa / (n + kappa)
+        weights = np.full(2 * n + 1, 1 / (2 * (n + kappa)))
+        weights[0] = kappa / (n + kappa)
 
-        return a
+        return weights
 
     def prediction(self,
                    control_vector: ArrayLike):
         
         u = np.asarray(control_vector, dtype = float).reshape(-1)
         a = self.get_weights()
-        n = self.mean.shape[0]
+        n = self.P.shape[0]
         
         # propagated sigma points
-        propagated_sigma_points = self.get_propagated_sigma_points(u) 
-        self.propagated_sigma_points = propagated_sigma_points     
+        sigma_points = self.get_sigma_points(self.mean, self.P)
+        propagated_sigma_points = np.array([self.f_model(point, u) for point in sigma_points])
 
-        # compute predicted mean   
+        # predicted mean   
         predicted_mean = a @ propagated_sigma_points
         
-        # compute predicted covariance
-        predicted_P = np.zeros_like(self.P)
-        for i in range(2 * n + 1):
-            diff = propagated_sigma_points[i, :] - predicted_mean
-            predicted_P += a[i] * np.outer(diff, diff)
+        # predicted covariance
+        predicted_P = np.zeros((n, n))
+        for weight, propagated_sigma_point in zip(a, propagated_sigma_points):
+            diff = propagated_sigma_point - predicted_mean
+            predicted_P += weight * np.outer(diff, diff)
 
         self.mean = predicted_mean
         self.P = predicted_P + self.Q
@@ -86,50 +77,42 @@ class UnscentedKalmanFilter:
 
     def correction(self,
                    measurement_vector: ArrayLike):
-
-        if self.propagated_sigma_points is None:
-            raise RuntimeError('Error: Prediction has not been preceded')
         
         y = np.asarray(measurement_vector, dtype = float).reshape(-1)
         n = self.mean.shape[0]
         m = y.size
         a = self.get_weights()
-        propagated_sigma_points = self.propagated_sigma_points
 
-        # predicted measurements from propagated sigma points
-        predicted_measurements = np.zeros((2 * n + 1, len(y)))
-        for i in range(2 * n + 1):
-            predicted_measurements[i, :] = self.h_model(propagated_sigma_points[i, :], 0)
+        # propagated sigma points
+        sigma_points = self.get_sigma_points(self.mean, self.P)
+        predicted_measurements = np.array([self.h_model(points) for points in sigma_points])
 
-        # estimate mean of predicted measurements
-        y_hat = a @ predicted_measurements
+        # mean of predicted measurements
+        predicted_measurements_mean = a @ predicted_measurements
 
-        # estimate covariance of predicted measurements
-        P_y = np.zeros((m,m), dtype = float)
-        for i in range(2 * n + 1):
-            diff = predicted_measurements[i, :] - y_hat
-            P_y += a[i] * np.outer(diff, diff)
-        
+        # covariance of predicted measurements
+        P_y = np.zeros((m, m))
+        for weight, predicted_measurement in zip(a, predicted_measurements):
+            diff = predicted_measurement - predicted_measurements_mean
+            P_y += weight * np.outer(diff, diff)
+
         P_y += self.R
         P_y = (P_y + P_y.T) / 2
 
-        # compute cross-covaraince
+        # cross-covariance
         P_xy = np.zeros((n, m))
-        for i in range(2 * n + 1):
-            diff_x = propagated_sigma_points[i, :] - self.mean
-            diff_y = predicted_measurements[i, :] - y_hat
-            P_xy += a[i] * np.outer(diff_x, diff_y)
+        for weight, sigma_point, predicted_measurement in zip(a, sigma_points, predicted_measurements):
+            P_xy += weight @ (sigma_point - self.mean) @ (predicted_measurement - predicted_measurements_mean).T
 
         # kalman gain
         A = P_y.T
         B = P_xy.T
         K = np.linalg.solve(A, B).T
 
-        # compute corrected mean & covariance
-        self.mean += K @ (y - y_hat)
+        # corrected mean & covariance
+        self.mean += K @ (y - predicted_measurements_mean)
         self.P -= K @ P_y @ K.T
         self.P = (self.P + self.P.T) / 2
-        self.propagated_sigma_points = None
 
     def unscentedkalmanfilter(self,
                               control_vector: ArrayLike,
