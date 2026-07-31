1eee9c14ab38d98d79491724678fba501f46687c
seoupark1 <seoupark05@gmail.com>
2026-07-31 14:54:44 +0000

    StateProvider를 활용한 control vector 반환 함수 제작

--- src/stochastic_control/control/lyapunov.py
+++ src/stochastic_control/control/lyapunov.py
@@ -1,17 +1,18 @@
 import numpy as np
-from numpy.typing import ArrayLike, NDArray
-from collections.abc import Callable
+from numpy.typing import ArrayLike
+
+from ..providers.state import MRPStateProvider
 from ..attitude.math import skew_symmetric
 from ..attitude.mrp import mrp_to_dcm, dcm_to_mrp
 
-class Lyapunov:
+class LyapunovControl:
 
     def __init__(self,
                  inertia_tensor: ArrayLike,
                  K: float,
                  P: ArrayLike,
-                 reference_provider: Callable,
-                 disturbance: Callable):
+                 reference_provider,
+                 disturbance):
 
         self.inertia_tensor = np.asarray(inertia_tensor, dtype = float).reshape(3,3)
         self.K = float(K)
@@ -19,14 +20,14 @@ class Lyapunov:
         self.reference_provider = reference_provider
         self.disturbance = disturbance
 
-    def lyapunov_function_mrp(self,
+    def mrp_lyapunov_function(self,
                               t: float,
-                              sigma_BN: ArrayLike,
-                              omega_BN_B: ArrayLike):
+                              state: ArrayLike):
 
         # body state
-        sigma_BN = np.asarray(sigma_BN, dtype = float).reshape(3)
-        omega_BN_B = np.asarray(omega_BN_B, dtype = float).reshape(3)
+        current_state = MRPStateProvider.build_context(t, state)
+        sigma_BN = np.asarray(dcm_to_mrp(current_state.attitude_BN), dtype = float).reshape(3)
+        omega_BN_B = np.asarray(current_state.angular_velocity_BN, dtype = float).reshape(3)
 
         # reference data
         sigma_RN_R, omega_RN_R, omega_RN_dot_R = self.reference_provider.get_reference(t)
@@ -46,14 +47,14 @@ class Lyapunov:
 
         return lyapunov_function
 
-    def control_vector_mrp(self,
+    def mrp_control_vector(self,
                            t: float,
-                           sigma_BN: ArrayLike,
-                           omega_BN_B: ArrayLike):
+                           state: ArrayLike):
         
         # body state
-        sigma_BN = np.asarray(sigma_BN, dtype = float).reshape(3)
-        omega_BN_B = np.asarray(omega_BN_B, dtype = float).reshape(3)
+        current_state = MRPStateProvider.build_context(t, state)
+        sigma_BN = np.asarray(dcm_to_mrp(current_state.attitude_BN), dtype = float).reshape(3)
+        omega_BN_B = np.asarray(current_state.angular_velocity_BN, dtype = float).reshape(3)
 
         # reference data
         sigma_RN_R, omega_RN_R, omega_RN_dot_R = self.reference_provider.get_reference(t)
@@ -64,9 +65,12 @@ class Lyapunov:
         dcm_BR = dcm_BN @ np.linalg.inv(dcm_RN)
 
         sigma_BR = dcm_to_mrp(dcm_BR)
-        omega_BR = omega_BN_B - dcm_BR @ omega_RN_R
+        omega_BR_B = omega_BN_B - dcm_BR @ omega_RN_R
         omega_RN_B = dcm_BR @ omega_RN_R
         omega_RN_dot_B = dcm_BR @ omega_RN_dot_R
 
         # compute control vector
         feedforward = self.inertia_tensor @ (omega_RN_dot_B - skew_symmetric(omega_BN_B) @ omega_RN_B) - self.disturbance.torque(t, state)
+        control_vector = -self.K * sigma_BR - self.P @ omega_BR_B + feedforward + skew_symmetric(omega_RN_B) @ self.inertia_tensor @ omega_RN_B
+        
+        return control_vector
