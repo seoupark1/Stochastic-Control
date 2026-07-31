6831f549afd5022a718d56b0db97ef4a0c3cb6cc
seoupark1 <seoupark05@gmail.com>
2026-07-31 14:16:38 +0000

    현재 body의 필요한 state 정보들만을 받아올 수 있는 클래스 제작

---
+++ src/stochastic_control/states/context.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+import numpy as np
+from numpy.typing import NDArray
+
+@dataclass(frozen = True)
+class StateContext:
+
+    position_N: NDArray[np.float64] = None
+    velocity_N: NDArray[np.float64] = None
+    attitude_BN: NDArray[np.float64] = None
+    angular_velocity_BN: NDArray[np.float64] = None
+
+
+
