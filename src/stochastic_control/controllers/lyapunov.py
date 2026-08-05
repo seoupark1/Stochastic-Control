5e2b3e36772bdc8c1f127e2d888208d43a6dde8e
seoupark1 <seoupark05@gmail.com>
2026-08-05 13:41:40 +0000

    controller가 estimated context와 disturbance만 받도록 수정

--- src/stochastic_control/controllers/lyapunov.py
+++ src/stochastic_control/controllers/lyapunov.py
@@ -4,33 +4,37 @@ from numpy.typing import ArrayLike
 from ..attitude.math import skew_symmetric
 from ..attitude.mrp import mrp_to_dcm, dcm_to_mrp
 
-class LyapunovControl:
+class LyapunovController:
 
     def __init__(self,
                  inertia_tensor: ArrayLike,
                  K: float,
                  P: ArrayLike,
                  reference_provider,
-                 disturbance):
+                 KI = None,
+                 estimated_disturbance_model = None):
 
         self.inertia_tensor = np.asarray(inertia_tensor, dtype = float).reshape(3,3)
         self.K = float(K)
+        self.KI = np.asarray(KI, dtype = float).reshape(3,3)
         self.P = np.asarray(P, dtype = float).reshape(3,3)
         self.reference_provider = reference_provider
-        self.disturbance = disturbance
+        self.disturbance_model = estimated_disturbance_model
 
     def mrp_lyapunov_function(self,
                               t: float,
-                              state: ArrayLike):
+                              estimated_rotational_state: ArrayLike):
 
-        current_state = np.asarray(state, dtype = float).reshape(6)
+        x_hat = np.asarray(estimated_rotational_state, dtype = float).reshape(6)
         
         # body state
-        sigma_BN = current_state[0:3]
-        omega_BN_B = current_state[3:6]
+        sigma_BN = x_hat[0:3]
+        omega_BN_B = x_hat[3:6]
 
-        # reference data
-        sigma_RN_R, omega_RN_R, omega_RN_dot_R = self.reference_provider.get_reference(t)
+        # reference state
+        reference = self.reference_provider.get_reference(t)
+        sigma_RN_R = reference.sigma_RN
+        omega_RN_R = reference.omega_RN_R
 
         # difference between body and reference
         dcm_BN = mrp_to_dcm(sigma_BN)
@@ -48,18 +52,25 @@ class LyapunovControl:
 
     def mrp_control_vector(self,
                            t: float,
-                           rotational_state: ArrayLike,
-                           context_builder):
+                           estimated_rotational_state: ArrayLike,
+                           estimated_context_builder):
 
-        rotational_state = np.asarray(rotational_state, dtype = float).reshape(6)
-        context = context_builder.build_context(t, rotational_state)
+        x_hat = np.asarray(estimated_rotational_state, dtype = float).reshape(6)
+        estimated_context = estimated_context_builder.build_context(t, x_hat)
+        estimated_disturbance = np.zeros(3)
+
+        if self.disturbance_model is not None:
+            estimated_disturbance = self.disturbance_model.torque(t, estimated_context)
         
         # body state
-        sigma_BN = rotational_state[0:3]
-        omega_BN_B = rotational_state[3:6]
+        sigma_BN = x_hat[0:3]
+        omega_BN_B = x_hat[3:6]
 
         # reference data
-        sigma_RN, omega_RN_R, omega_RN_dot_R = self.reference_provider.get_reference(t)
+        reference = self.reference_provider.get_reference(t)
+        sigma_RN = reference.sigma_RN
+        omega_RN_R = reference.omega_RN_R
+        omega_RN_dot_R = reference.omega_RN_dot_R
 
         # difference between body and reference
         dcm_BN = mrp_to_dcm(sigma_BN)
@@ -72,7 +83,7 @@ class LyapunovControl:
         omega_RN_dot_B = dcm_BR @ omega_RN_dot_R
 
         # compute control vector
-        feedforward = self.inertia_tensor @ (omega_RN_dot_B - skew_symmetric(omega_BN_B) @ omega_RN_B) - self.disturbance.torque(t, context)
+        feedforward = self.inertia_tensor @ (omega_RN_dot_B - skew_symmetric(omega_BN_B) @ omega_RN_B) - estimated_disturbance
         control_vector = -self.K * sigma_BR - self.P @ omega_BR_B + feedforward + skew_symmetric(omega_RN_B) @ self.inertia_tensor @ omega_RN_B
         
         return control_vector
