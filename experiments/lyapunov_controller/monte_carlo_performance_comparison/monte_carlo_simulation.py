ce09824869dea1af3284dc0b90013df2bade141c
seoupark1 <seoupark05@gmail.com>
2026-08-08 13:33:18 +0000

    update

--- experiments/lyapunov_controller/monte_carlo_performance_comparison/monte_carlo_simulation.py
+++ experiments/lyapunov_controller/monte_carlo_performance_comparison/monte_carlo_simulation.py
@@ -147,7 +147,7 @@ def simulate_integral_controller(K, P, KI):
 
 
 # monte carlo simulation
-num_simulations = 20
+num_simulations = 50
 steady_state_start = 250.0
 standard_sigma_rms_history = np.zeros(num_simulations)
 standard_omega_rms_history = np.zeros(num_simulations)
