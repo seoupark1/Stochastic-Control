fa80edb578cc777acf105e16194db8f7e3afb931
seoupark1 <seoupark05@gmail.com>
2026-08-08 04:26:07 +0000

    integral controller의 state는 sigma, omega, eta의 9개 구성으로 확장

--- experiments/lyapunov_controller/compare_standard_integral_performances/compare_standard_integral_performances.py
+++ experiments/lyapunov_controller/compare_standard_integral_performances/compare_standard_integral_performances.py
@@ -7,7 +7,7 @@ from stochastic_control.controllers.lyapunov import LyapunovController
 from stochastic_control.providers.attitude_reference import MRPReferenceProvider
 from stochastic_control.providers.state import MRPStateProvider
 
-from stochastic_control.attitude.mrp import mrp_b_matrix
+from stochastic_control.attitude.mrp import mrp_b_matrix, mrp_derivative
 from stochastic_control.attitude.math import skew_symmetric
 
 def mrp_b_derivative(sigma, sigma_dot):
@@ -50,6 +50,7 @@ inertia_tensor = np.diag([100, 75, 80])
 sigma_BN_B_0 = np.array([0.1, 0.2, -0.1])
 omega_BN_B_0 = np.radians(np.array([3, 1, -2]))
 initial_rotational_state = np.concatenate((sigma_BN_B_0, omega_BN_B_0))
+initial_state_for_integral_controller = np.concatenate((sigma_BN_B_0, omega_BN_B_0, np.zeros(3)))
 control_gain = 5
 damping_matrix = 10 * np.eye(3)
 integral_control_gain = 0.005
@@ -62,6 +63,8 @@ standard_controller = LyapunovController(inertia_tensor,
                                          damping_matrix, 
                                          reference_provider,
                                          estimated_disturbance_model = None)
+
+_, _, omega_BR_B_0 = standard_controller.mrp_tracking_error(0, initial_rotational_state)
 
 integral_controller = LyapunovController(inertia_tensor, 
                                          control_gain, 
@@ -91,6 +94,33 @@ standard_sol = solve_ivp(performance_of_standard_controller,
                          method = 'RK45',
                          t_eval = teval)
 
+def performance_of_integral_controller(t,
+                                       estimated_state):
+    
+    estimated_rotational_state = estimated_state[0:6]
+    sigma_BN_B = estimated_state[0:3]
+    omega_BN_B = estimated_state[3:6]
+    integral_state = estimated_state[6:9]
+    
+    control_vector, eta_dot = integral_controller.mrp_integral_control_vector(t,
+                                                                              estimated_rotational_state,
+                                                                              integral_state,
+                                                                              omega_BR_B_0,
+                                                                              state_provider)
+
+    total_torque = control_vector + unknown_disturbance
+
+    sigma_dot = mrp_derivative(sigma_BN_B, omega_BN_B)
+    omega_dot = rigid_body.angular_acceleration(omega_BN_B, total_torque)
+
+    return np.concatenate((sigma_dot, omega_dot, eta_dot))
+
+integral_sol = solve_ivp(performance_of_integral_controller,
+                         t_span = tspan,
+                         y0 = initial_state_for_integral_controller,
+                         method = 'RK45',
+                         t_eval = teval)
+    
 def get_graph(time,
               history,
               xlabel: str,
@@ -110,9 +140,23 @@ def get_graph(time,
     plt.savefig('experiments/lyapunov_controller/compare_standard_integral_performances/' + png_title)
     plt.close()
 
+def get_tracking_error_history(sol,
+                               sigma_BR_history,
+                               omega_BR_history):
+
+    for i, t in enumerate(sol.t):
+        rotational_state = sol.y[0:6, i]
+
+        dcm_BR_B, sigma_BR_B, omega_BR_B = standard_controller.mrp_tracking_error(t, rotational_state)
+        sigma_BR_history[:, i] = sigma_BR_B
+        omega_BR_history[:, i] = omega_BR_B
+
+    return sigma_BR_history, omega_BR_history
+
 # standard controller body attitude
 sigma_history = standard_sol.y[0:3]
 omega_history = standard_sol.y[3:6]
+
 get_graph(standard_sol.t, 
           sigma_history, 
           'time', 
@@ -130,18 +174,15 @@ get_graph(standard_sol.t,
           'standard_controller_body_angular_velocity.png')
 
 # standard controller tracking error
-sigma_BR_history = np.zeros((3, len(standard_sol.t)))
-omega_BR_history = np.zeros((3, len(standard_sol.t)))
+sigma_BR_history_1 = np.zeros((3, len(standard_sol.t)))
+omega_BR_history_1 = np.zeros((3, len(standard_sol.t)))
 
-for i, t in enumerate(standard_sol.t):
-    rotational_state = standard_sol.y[:, i]
-
-    dcm_BR_B, sigma_BR_B, omega_BR_B = standard_controller.mrp_tracking_error(t, rotational_state)
-    sigma_BR_history[:, i] = sigma_BR_B
-    omega_BR_history[:, i] = omega_BR_B
+sigma_BR_history_1, omega_BR_history_1 = get_tracking_error_history(standard_sol,
+                                                                    sigma_BR_history_1,
+                                                                    omega_BR_history_1)
 
 get_graph(standard_sol.t, 
-          sigma_BR_history, 
+          sigma_BR_history_1, 
           'time', 
           'attitude error', 
           ['sigma_BR_1', 'sigma_BR_2', 'sigma_BR_3'],
@@ -149,9 +190,53 @@ get_graph(standard_sol.t,
           'standard_controller_attitude_error.png')
 
 get_graph(standard_sol.t, 
-          omega_BR_history, 
+          omega_BR_history_1, 
           'time', 
           'angular velocity error', 
           ['omega_BR_1', 'omega_BR_2', 'omega_BR_3'],
           'Standard Controller', 
           'standard_controller_angular_velocity_error.png')
+
+# integral controller body attitude
+sigma_history = integral_sol.y[0:3]
+omega_history = integral_sol.y[3:6]
+
+get_graph(integral_sol.t, 
+          sigma_history, 
+          'time', 
+          'body attitude', 
+          ['sigma_1', 'sigma_2', 'sigma_3'],
+          'Integral Controller', 
+          'integral_controller_body_attitude.png')
+
+get_graph(integral_sol.t, 
+          omega_history, 
+          'time', 
+          'body angular velocity', 
+          ['omega_1', 'omega_2', 'omega_3'],
+          'Integral Controller', 
+          'integral_controller_body_angular_velocity.png')
+
+# integral controller tracking error
+sigma_BR_history_2 = np.zeros((3, len(integral_sol.t)))
+omega_BR_history_2 = np.zeros((3, len(integral_sol.t)))
+
+sigma_BR_history_2, omega_BR_history_2 = get_tracking_error_history(integral_sol,
+                                                                    sigma_BR_history_2,
+                                                                    omega_BR_history_2)
+
+get_graph(integral_sol.t, 
+          sigma_BR_history_2, 
+          'time', 
+          'attitude error', 
+          ['sigma_BR_1', 'sigma_BR_2', 'sigma_BR_3'],
+          'Integral Controller', 
+          'integral_controller_attitude_error.png')
+
+get_graph(integral_sol.t, 
+          omega_BR_history_2, 
+          'time', 
+          'angular velocity error', 
+          ['omega_BR_1', 'omega_BR_2', 'omega_BR_3'],
+          'Integral Controller', 
+          'integral_controller_angular_velocity_error.png')
