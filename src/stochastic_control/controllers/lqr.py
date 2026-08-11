4cec96db3e7be6bb3992dad643af752d54029456
seoupark1 <seoupark05@gmail.com>
2026-08-11 11:44:58 +0000

    Infinite-horizon, Finite-horizon 등 LQR 기능에 따라 controller class 분리

--- src/stochastic_control/controllers/lqr.py
+++ src/stochastic_control/controllers/lqr.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.linalg import solve_continuous_are
 
-class LQRController:
+class InfiniteHorizonLQRController:
 
     def __init__(self, A, B, Q, R):
 
@@ -12,3 +12,16 @@ class LQRController:
                        error_state):
 
         return -self.K @ error_state
+
+class FiniteHorizonLQRController:
+
+    def __init__(self, A, B, Q, R, Qf):
+
+        self.Q = Q
+        if self.Q != self.Q.T:
+            raise ValueError('Q matrix should be ')
+        
+        self.Qf = Qf
+
+
+'''A, B, Q, R, Qf 행렬이 만족해야 하는 symmetric positive definite 혹은 semi-definite 처음에 확인, differential ricatti eqn은 따로 구해야함'''
