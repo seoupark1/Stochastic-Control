da504f006c328dd8705eea58c8efaec12a9f9614
seoupark1 <seoupark05@gmail.com>
2026-08-11 13:55:30 +0000

    finite horizon 구현, solve_ivp를 사용하게 위해 S를 flatten

--- src/stochastic_control/controllers/lqr.py
+++ src/stochastic_control/controllers/lqr.py
@@ -1,5 +1,11 @@
 import numpy as np
-from scipy.linalg import solve_continuous_are
+from numpy.typing import ArrayLike
+
+from scipy.linalg import solve_continuous_are, inv
+from scipy.integrate import solve_ivp
+
+def is_SPD(matrix):
+
 
 class InfiniteHorizonLQRController:
 
@@ -15,13 +21,71 @@ class InfiniteHorizonLQRController:
 
 class FiniteHorizonLQRController:
 
-    def __init__(self, A, B, Q, R, Qf):
+    def __init__(self, 
+                 A: ArrayLike, 
+                 B: ArrayLike, 
+                 Q: ArrayLike, 
+                 R: ArrayLike, 
+                 Qf: ArrayLike, 
+                 tf: float):
 
-        self.Q = Q
-        if self.Q != self.Q.T:
-            raise ValueError('Q matrix should be ')
-        
-        self.Qf = Qf
+        self.A = np.asarray(A, dtype = float)
+        self.B = np.asarray(B, dtype = float)
+        self.Q = np.asarray(Q, dtype = float)
+        self.R = np.asarray(R, dtype = float)
+        self.Qf = np.asarray(Qf, dtype = float)
+
+        self.tf = float(tf)
+
+        # SPD 검증 코드
+        #
+        #
+        #
 
 
-'''A, B, Q, R, Qf 행렬이 만족해야 하는 symmetric positive definite 혹은 semi-definite 처음에 확인, differential ricatti eqn은 따로 구해야함'''
+    def differential_ricatti_equation(self,
+                                      current_S: ArrayLike):
+
+        S = np.asarray(current_S, dtype = float)
+        S = (S + S.T) / 2
+
+        S_dot = - (S @ self.A + self.A.T @ S - S @ self.B @ inv(self.R) @ self.B.T @ S + self.Q)
+
+        return S_dot.reshape(-1)
+
+    def get_S(self,
+              current_t: float):
+
+        # time conditions
+        current_t = float(current_t)
+        t_span = (self.tf, current_t)
+        t_eval = [current_t]
+
+        # S conditions
+        initial_S = self.Qf
+        size_S = self.Q.shape[0]
+
+        sol = solve_ivp(func = self.differential_ricatti_equation, 
+                        t_span = t_span, 
+                        y0 = initial_S, 
+                        method = 'RK45', 
+                        t_eval = t_eval)
+
+        current_S = sol.y[:, 1].reshape(size_S, size_S)
+        current_S = (current_S + current_S.T) / 2
+
+        return current_S
+
+    def control_vector(self, 
+                       t: float,
+                       estimated_state: ArrayLike):
+
+        # check parameters
+        t = float(t)
+        x_hat = np.asarray(estimated_state, dtype = float)
+
+        # get control vector
+        S = self.get_S(t)
+        control_vector = -inv(self.R) @ self.B.T @ S @ x_hat
+
+        return control_vector
