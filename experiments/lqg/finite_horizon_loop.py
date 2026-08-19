f1d0ff1f12b2252d7826761d29d4e52f417443c3
seoupark1 <seoupark05@gmail.com>
2026-08-19 14:53:12 +0000

    motion, measurement dynamics에 Gaussian noise 추가 & lqr이 kalman filter보다 먼저 수행되도록 순서 변경 & nominal state는 [0, 0]

--- experiments/lqg/finite_horizon_loop.py
+++ experiments/lqg/finite_horizon_loop.py
@@ -6,35 +6,28 @@ from stochastic_control.controllers.lqr.discrete_time_finite_horizon import Disc
 from stochastic_control.controllers.lqg.discrete_time_finite_horizon import DiscreteTimeFiniteHorizonLQGController
 from stochastic_control.noises.gaussian_noise import GaussianNoise
 
-def true_state_provider(t):
-
-    position = (1/3) * t**3 + 4 * t**2 + 5 * t + 6
-    velocity = t**2 + 8 * t + 5
-
-    return np.array([position, velocity])
-
 def simulation():
 
     dt = 0.1 # [s]
 
     # set estimator
-    initial_state = np.array([[0], 
-                              [5]])
+    state = np.array([[0], 
+                      [5]])
     covariance = np.array([[0.01, 0],
                            [0, 1]])
-    motion_jacobian = np.array([[1, dt],
-                                [0, 1]])
-    control_jacobian = np.array([[0],
-                                 [1]])
-    measurement_jacobian = np.array([[1, 0]])
+    A = np.array([[1, dt],
+                  [0, 1]])
+    B = np.array([[dt**2 / 2],
+                  [dt]])
+    H = np.array([[1, 0]])
     motion_noise_covariance = 5 * np.eye(2)
     measurement_noise_covariance = np.array([[10]])
 
-    kalmanfilter = KalmanFilter(state = initial_state,
+    kalmanfilter = KalmanFilter(state = state,
                                 covariance = covariance,
-                                motion_jacobian = motion_jacobian,
-                                control_jacobian = control_jacobian,
-                                measurement_jacobian = measurement_jacobian,
+                                motion_jacobian = A,
+                                control_jacobian = B,
+                                measurement_jacobian = H,
                                 motion_noise_covariance = motion_noise_covariance,
                                 measurement_noise_covariance = measurement_noise_covariance)
 
@@ -43,10 +36,9 @@ def simulation():
     R = np.eye(1)
     Qf = np.diag([10, 1])
     N = 100
-    u_cmd = np.array([[-2]])
 
-    lqr = DiscreteTimeFiniteHorizonLQRController(A = motion_jacobian,
-                                                 B = control_jacobian,
+    lqr = DiscreteTimeFiniteHorizonLQRController(A = A,
+                                                 B = B,
                                                  Q = Q,
                                                  R = R,
                                                  Qf = Qf,
@@ -54,66 +46,76 @@ def simulation():
     
     lqg = DiscreteTimeFiniteHorizonLQGController(kalmanfilter, lqr)
 
-    # sensor property
+    # noise models
     rng = np.random.default_rng(seed = 2026)
-    mean = np.array([[0]])
-    noise = GaussianNoise(mean, measurement_noise_covariance)
+    motion_noise = GaussianNoise(np.zeros(2), motion_noise_covariance)
+    measurement_noise = GaussianNoise(np.zeros(1), measurement_noise_covariance)
 
     # history
-    time_step = np.zeros(N)
-    measured_gps_history = np.zeros(N)
-    nominal_position_history = np.zeros(N)
+    time = np.zeros(N)
+    true_state_history = np.zeros((2, N))
     estimated_state_history = np.zeros((2, N))
-    control_vector_history = np.zeros(N)
+    measurement_history = np.zeros(N)
+    control_history = np.zeros(N)
+
+    # initial
+    x_true = np.array([5, -3])
 
     for k in range(N):
 
-        t = interval * k
-        time_step[k] = t
+        u_cmd = lqg.control_vector(k)
+        control_history[k] = u_cmd
 
-        # get measured sensor(gps) data
-        nominal_position_history[k] = true_state_provider(t)[0]
-        measured_position_vector = nominal_position_history[k] + noise.get_sample(rng)[0]
-        measured_gps_history[k] = measured_position_vector
+        # motion
+        w = motion_noise.get_sample(rng)
+        x_true = A @ x_true + B @ u_cmd + w
+        true_state_history[:, k] = x_true
+
+        # measurement
+        v = measurement_noise.get_sample(rng)
+        y = H @ x_true + v
+        measurement_history[k] = y
 
         # estimate current state
-        lqg.estimate(u_cmd, measured_position_vector)
-        estimated_state_history[0:2, k] = kalmanfilter.x
+        lqg.estimate(u_cmd, y)
+        estimated_state_history[:, k] = kalmanfilter.x
 
-        # return control vector
-        u_cmd = lqg.control_vector(k)
-        control_vector_history[k] = u_cmd
-
-    # plot sensor noise
-    plt.plot(time_step, nominal_position_history, label = 'true position')
-    plt.plot(time_step, measured_gps_history, label = 'gps data')
+    # true vs estimated
+    plt.subplot(2, 1, 1)
+    plt.plot(time, true_state_history[:, 0], label = 'true position')
+    plt.plot(time, estimated_state_history[:, 0], label = 'estimated position')
     plt.xlabel('time [s]')
     plt.ylabel('position [m]')
+    plt.title('Position Comparison')
     plt.legend()
     plt.grid(True)
-    plt.savefig('experiments/lqg/sensor_noise.png')
+    plt.subplot(2, 1, 2)
+    plt.plot(time, true_state_history[:, 1], label = 'true velocity')
+    plt.plot(time, estimated_state_history[:, 1], label = 'estimated velocity')
+    plt.xlabel('time [s]')
+    plt.ylabel('velocity [m/s]')
+    plt.title('Velocity Comparison')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('experiments/lqg/true_vs_estimated.png')
     plt.close()
 
-    # plot estimated state & control torque
-    plt.subplot(3, 1, 1)
-    plt.plot(time_step, estimated_state_history[0:3, ])
+    # measurement y & control u
+    plt.subplot(2, 1, 1)
+    plt.plot(time, measurement_history)
     plt.xlabel('time [s]')
-    plt.ylabel('estimated position [m]')
+    plt.ylabel('measured position [m]')
+    plt.title('Measurement History')
     plt.legend()
     plt.grid(True)
-    plt.subplot(3, 1, 2)
-    plt.plot(time_step, estimated_state_history[3:6, ])
+    plt.subplot(2, 1, 2)
+    plt.plot(time, control_history)
     plt.xlabel('time [s]')
-    plt.ylabel('estimated velocity [m/s]')
+    plt.ylabel('torque [Nm]')
+    plt.title('Control History')
     plt.legend()
     plt.grid(True)
-    plt.subplot(3, 1, 3)
-    plt.plot(time_step, control_vector_history, label = 'control vector')
-    plt.xlabel('time [s]')
-    plt.ylabel('control torque [Nm]')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig('experiments/lqg/estimated_state_and_control_torque.png')
+    plt.savefig('experiments/lqg/measurement_and_control.png')
     plt.close()
 
 simulation()
