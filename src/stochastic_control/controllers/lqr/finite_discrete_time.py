fb11ca152cc3d64c83c957244727b4c19fb446d1
seoupark1 <seoupark05@gmail.com>
2026-08-19 11:03:14 +0000

    since S, K matrix is not scalar replace self.S, self.K to 3D array

--- src/stochastic_control/controllers/lqr/finite_discrete_time.py
+++ src/stochastic_control/controllers/lqr/finite_discrete_time.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy.typing import ArrayLike
 from numpy.linalg import inv
 
-from stochastic_control.math_tools import is_PSD
+from stochastic_control.math_tools import is_PSD, is_SPD
 
 class DiscreteTimeFiniteHorizonLQRController:
 
@@ -13,29 +13,36 @@ class DiscreteTimeFiniteHorizonLQRController:
                  Q: ArrayLike,
                  R: ArrayLike,
                  Qf: ArrayLike,
-                 N: float):
+                 N: int):
 
         self.A = np.asarray(A, dtype = float)
         self.B = np.asarray(B, dtype = float)
-
-        matrix_list = [Q, R, Qf]
-
-        # check symmetric positive semi-definite
-        for matrix in matrix_list:
-            if not np.allclose(matrix, matrix.T):
-                raise ValueError(f'{matrix} must be symmetric')
-            
-            if not is_PSD(matrix):
-                raise ValueError(f'{matrix} must be positive semi-definite')
-
         self.Q = np.asarray(Q, dtype = float)
         self.R = np.asarray(R, dtype = float)
         self.Qf = np.asarray(Qf, dtype = float)
-        self.N = float(N)
+        self.N = int(N)
 
+        # check symmetric positive (semi) definite
+        for name, matrix in (('Q', self.Q), ('Qf', self.Qf), ('R', self.R)):
+            # symmetry
+            if not np.allclose(matrix, matrix.T):
+                raise ValueError(f'{name} must be symmetric')
+
+            # positive (semi) definite
+            if name == 'R':
+                if not is_SPD(R):
+                    raise ValueError('R must be positive semi-definite')
+
+            else: 
+                if not is_PSD(matrix):
+                    raise ValueError(f'{name} must be positive semi-definite')
+                
         # make S & K list
-        self.S = np.zeros(self.N + 1)
-        self.K = np.zeros(self.N)
+        n = self.A.shape[0]
+        m = self.B.shape[1]
+
+        self.S = np.zeros((self.N + 1, n, n))
+        self.K = np.zeros((self.N, m, n))
         self.S[N] = self.Qf
 
         for k in range(N-1, -1, -1):
@@ -48,9 +55,9 @@ class DiscreteTimeFiniteHorizonLQRController:
             self.K[k] = inv(self.R + self.B.T @ self.S[k+1] @ self.B) @ self.B.T @ self.S[k+1] @ self.A
 
     def control_vector(self,
-                       k_step: float,
+                       k_step: int,
                        estimated_state: ArrayLike):
 
-        x_hat = np.asarray(estimated_state, dtype = float)
+        x_hat = np.asarray(estimated_state, dtype = float).reshape(-1)
 
         return -self.K[k_step] @ x_hat
