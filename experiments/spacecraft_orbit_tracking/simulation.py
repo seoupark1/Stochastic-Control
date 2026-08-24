cef4bea04b050fb2625f8757fb85a0dd14f94630
seoupark1 <seoupark05@gmail.com>
2026-08-24 14:57:44 +0000

    0부터 tf까지의 전체 시간에 대해 star tracker와 gyroscope의 sampling rate을 반영한 correction 진행 & simulation output을 dictionary로 받아 순서의 제약을 받지 않도록 수정

--- experiments/spacecraft_orbit_tracking/simulation.py
+++ experiments/spacecraft_orbit_tracking/simulation.py
@@ -18,13 +18,25 @@ from stochastic_control.estimators.extended_kalman_filter import ExtendedKalmanF
 from stochastic_control.controllers.lqr.local_trajectory_stabilization import LocalTrajectoryStabilizationLQRController
 from stochastic_control.compensators.nonlinear_compensator.ekf_lqr import EKFLQRCompensator
 
-def simulation(x_true: ArrayLike,
+def simulation(initial_x_true: ArrayLike,
                initial_x_hat : ArrayLike,
                initial_covariance : ArrayLike,
+               motion_noise_covariance: ArrayLike,
+               star_tracker_noise_covariance: ArrayLike,
+               gyroscope_noise_covariance: ArrayLike,
+               tf: float,
+               star_tracker_sampling_rate: float,
+               gyroscope_sampling_rate: float,
                control_limiter = None):
 
+    # sampling period
     dt = 0.01
-    inertia_tensor = np.diag([0.2507, 0.2507, 0.0136])
+    star_tracker_dt = 1 / star_tracker_sampling_rate
+    gyroscope_dt = 1 / gyroscope_sampling_rate
+    star_tracker_time = 0
+    gyroscope_time = 0
+
+    inertia_tensor = np.diag([1448.3, 1346.2, 689.8])
     mu = 4.2828 * 10**13
 
     gravity_gradient = GravityGradient(inertia_tensor = inertia_tensor,
@@ -145,26 +157,13 @@ def simulation(x_true: ArrayLike,
     
     def measurement_jacobian(t, state):
 
-        H = approx_derivative(fun = lambda x: measurement_model(t, x),
-                              x0 = state,
-                              method = '3-point')
+        return approx_derivative(fun = lambda x: measurement_model(t, x),
+                                 x0 = state,
+                                 method = '3-point')
 
-        return H
-
-    # initial true tracking error
-    x_true = np.array([0.03, -0.03, -0.01, np.deg2rad(-5), np.deg2rad(-4), np.deg2rad(3)])
-
-    # ekf properties (trustworthy case)
-    mrp_std = np.tan(np.deg2rad(10) / 4) / np.sqrt(3)
-    omega_std = np.deg2rad(5) / np.sqrt(3)
-
-    initial_x_hat = x_true + np.array([-0.02, 0.02, -0.01, np.deg2rad(3), np.deg2rad(-2), np.deg2rad(-1.5)])
-    initial_covariance = np.diag([mrp_std**2, mrp_std**2, mrp_std**2, omega_std**2, omega_std**2, omega_std**2])
+    # ekf properties
     motion_noise_jacobian = np.eye(6)
     measurement_noise_jacobian = np.eye(6)
-    motion_noise_covariance = np.diag([1e-9, 1e-9, 1e-9, 1e-7, 1e-7, 1e-7])
-    star_tracker_noise_covariance = np.diag([5.88 * 1e-10, 5.88 * 1e-10, 5.88 * 1e-10])
-    gyroscope_noise_covariance = np.diag([1.5 * 1e-4, 1.5 * 1e-4, 1.5 * 1e-4])
     measurement_noise_covariance = np.block([[star_tracker_noise_covariance, np.zeros((3, 3))],
                                              [np.zeros((3, 3)), gyroscope_noise_covariance]])
 
@@ -182,7 +181,7 @@ def simulation(x_true: ArrayLike,
     Q = np.diag([50, 50, 50, 10, 10, 10])
     R = 5 * np.eye(3)
     Qf = 10 * Q
-    tf = 30
+    x_true = initial_x_true
 
     lqr = LocalTrajectoryStabilizationLQRController(Q = Q,
                                                     R = R,
@@ -196,8 +195,8 @@ def simulation(x_true: ArrayLike,
     # noises & sensors
     rng = np.random.default_rng(seed = 2026)
     motion_noise_provider = GaussianNoise(np.zeros(6), motion_noise_covariance)
-    star_tracker = StarTracker(np.zeros(3), measurement_noise_covariance[0:3, 0:3])
-    gyroscope = Gyroscope(np.zeros(3), measurement_noise_covariance[3:6, 3:6])
+    star_tracker = StarTracker(np.zeros(3), star_tracker_noise_covariance)
+    gyroscope = Gyroscope(np.zeros(3), gyroscope_noise_covariance)
 
     # total steps
     total_step = int(tf / dt)
@@ -226,8 +225,8 @@ def simulation(x_true: ArrayLike,
     # run simulation
     for k in range(total_step):
 
-        # time
         t = k * dt
+        next_t = time[k + 1]
 
         # control with saturation
         u_cmd = compensator.control_vector(t)
@@ -238,33 +237,61 @@ def simulation(x_true: ArrayLike,
         else: 
             u_actual = control_limiter.saturation(u_cmd)
 
-        cmd_control_history[:, k] = u_cmd
-        actual_control_history[:, k] = u_actual
-
-        # true value mrp shadow set transfer
+        # true state propagation & mrp shadow set transfer
         x_true = motion_model(t, x_true, u_actual) + motion_noise_provider.get_sample(rng)
         x_true[0:3] = mrp_shadow_set(x_true[0:3])
 
-        reference_state_history[:, k + 1] = reference_x_function(time[k + 1])
+        # prediction
+        ekf.prediction(u_actual, next_t)
+
+        # create each sensor's jacobian and model for different sampling
+        star_tracker_jacobian = measurement_jacobian(next_t, x_true)[0:3, :]
+        gyroscope_jacobian = measurement_jacobian(next_t, x_true)[3:6, :]
+        star_tracker_measurement_model = measurement_model(next_t, x_true)[0:3]
+        gyroscope_measurement_model = measurement_model(next_t, x_true)[3:6]
+        measured_y = np.zeros(6)
+
+        # measure attitude & correction
+        if next_t >= star_tracker_time:
+        
+            ideal_attitude = star_tracker_measurement_model(next_t, x_true)
+            y = star_tracker.measure(ideal_attitude, rng)
+            measured_y[0:3] = y
+
+            ekf.correction(measurement_vector = y,
+                           t = next_t,
+                           measurement_model = star_tracker_measurement_model,
+                           measurement_jacobian = star_tracker_jacobian,
+                           measurement_covariance = star_tracker_noise_covariance)
+            
+            ekf.x[0:3] = mrp_shadow_set(ekf.x[0:3])
+            star_tracker_time += star_tracker_dt
+
+        # measure omega & correction
+        if next_t >= gyroscope_time:
+
+            ideal_omega = gyroscope_measurement_model(next_t, x_true)
+            y = gyroscope.measure(ideal_omega, rng)
+            measured_y[3:6] = y
+
+            ekf.correction(measurement_vector = y,
+                           t = next_t,
+                           measurement_model = gyroscope_measurement_model,
+                           measurement_jacobian = gyroscope_jacobian,
+                           measurement_covariance = gyroscope_noise_covariance)
+
+            ekf.x[0:3] = mrp_shadow_set(ekf.x[0:3])
+            gyroscope_time += gyroscope_dt
+
+        # update history
         true_state_history[:, k + 1] = x_true
-
-        # measurement using star tracker & gyroscope
-        ideal_y = measurement_model(time[k + 1], x_true)
-        ideal_attitude = ideal_y[0:3]
-        ideal_omega = ideal_y[3:6]
-
-        measured_attitude = star_tracker.measure(ideal_attitude, rng)
-        measured_omega = gyroscope.measure(ideal_omega, rng)
-
-        y = np.concatenate((measured_attitude, measured_omega))
-        measurement_history[:, k] = y
-
-        # estimate
-        compensator.estimate(time[k + 1], u_cmd, y)
-        ekf.x[0:3] = mrp_shadow_set(ekf.x[0:3])
+        reference_state_history[:, k + 1] = reference_x_function(next_t)
         estimated_state_history[:, k + 1] = ekf.x
+        cmd_control_history[:, k] = u_cmd
+        actual_control_history[:, k] = u_actual
+        measurement_history[:, k] = measured_y
 
-    # compute tracking & estimation error
+    # compute tracking error & estimation error
     for k in range(total_step + 1):
 
         sigma_BR = true_state_history[0:3, k]
@@ -273,7 +300,7 @@ def simulation(x_true: ArrayLike,
         sigma_BhatR = estimated_state_history[0:3, k]
         omega_BhatR_Bhat = estimated_state_history[3:6, k]
 
-        # angle difference
+        # principal angle difference
         dcm_BR = mrp_to_dcm(sigma_BR)
         dcm_BhatR = mrp_to_dcm(sigma_BhatR)
         dcm_BhatB = dcm_BhatR @ dcm_BR.T
@@ -292,24 +319,39 @@ def simulation(x_true: ArrayLike,
         attitude_estimation_error_angle_history[k] = angle_BhatB
         omega_estimation_error_history[:, k] = omega_BhatB_B
 
-    # omega estimation norm error
+    # omega estimation error norm
     omega_estimation_norm_error_history = np.linalg.norm(omega_estimation_error_history, axis = 0)
 
     # omega tracking error
     omega_tracking_error_norm_history = np.linalg.norm(true_state_history[3:6, :], axis = 0)
 
-    return (time,
-            true_state_history,
-            reference_state_history,
-            estimated_state_history,
-            cmd_control_history,
-            actual_control_history,
-            measurement_history,
-            attitude_tracking_error_norm_history,
-            omega_tracking_error_norm_history,
-            attitude_estimation_error_angle_history,
-            omega_estimation_norm_error_history)
+    return {'time': time,
+            'true_state': true_state_history,
+            'reference_state': reference_state_history,
+            'estimated_state': estimated_state_history,
+            'commanded_control': cmd_control_history,
+            'actual_control': actual_control_history,
+            'measurement': measurement_history,
+            'attitude_tracking_error': attitude_tracking_error_norm_history,
+            'omega_tracking_error': omega_tracking_error_norm_history,
+            'attitude_estimation_error': attitude_estimation_error_angle_history,
+            'omega_estimation_error': omega_estimation_norm_error_history}
 
+
+
+# initial true tracking error
+x_true = np.array([0.03, -0.03, -0.01, np.deg2rad(-5), np.deg2rad(-4), np.deg2rad(3)])
+
+# ekf properties (trustworthy case)
+mrp_std = np.tan(np.deg2rad(10) / 4) / np.sqrt(3)
+omega_std = np.deg2rad(5) / np.sqrt(3)
+
+initial_x_hat = x_true + np.array([-0.02, 0.02, -0.01, np.deg2rad(3), np.deg2rad(-2), np.deg2rad(-1.5)])
+initial_covariance = np.diag([mrp_std**2, mrp_std**2, mrp_std**2, omega_std**2, omega_std**2, omega_std**2])
+
+motion_noise_covariance = np.diag([1e-9, 1e-9, 1e-9, 1e-7, 1e-7, 1e-7])
+star_tracker_noise_covariance = np.diag([5.88 * 1e-10, 5.88 * 1e-10, 5.88 * 1e-10])
+gyroscope_noise_covariance = np.diag([1.5 * 1e-4, 1.5 * 1e-4, 1.5 * 1e-4])
 
 
 # tracking error
