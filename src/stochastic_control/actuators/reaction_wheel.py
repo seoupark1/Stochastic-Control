42e6f3e2cf1255b3d0b392b8a1e8068e7f1673e7
seoupark1 <seoupark05@gmail.com>
2026-08-24 13:24:07 +0000

    np.clip으로 torque saturation 구현

--- src/stochastic_control/actuators/reaction_wheel.py
+++ src/stochastic_control/actuators/reaction_wheel.py
@@ -9,4 +9,8 @@ class ReactionWheel:
         self.max_torque = np.asarray(max_torque, dtype = float).reshape(-1)
 
     def saturation(self,
-                   )
+                   control_torque: ArrayLike):
+
+        control_torque = np.asarray(control_torque, dtype = float).reshape(-1)
+
+        return np.clip(control_torque, -self.max_torque, self.max_torque)
