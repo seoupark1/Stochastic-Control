91ad4ce5ca25b420106be15fbc9a9060bff9df0a
seoupark1 <seoupark05@gmail.com>
2026-08-30 10:49:23 +0000

    sampling rate으로 measurement 진행이 되지 않았을 경우를 대비해 estimate에서 correction을 선택적으로 수행

--- src/stochastic_control/compensators/ekf_nmpc.py
+++ src/stochastic_control/compensators/ekf_nmpc.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from stochastic_control.controllers.mpc.nmpc import NMPCController
 from stochastic_control.estimators.extended_kalman_filter import ExtendedKalmanFilter
 
@@ -15,23 +13,21 @@ class EKFNMPCCompensator:
     def estimate(self,
                  t,
                  control_vector,
-                 measurement_vector):
+                 measurement_vector = None):
 
         self.estimator.prediction(control_vector, t)
-        self.estimator.correction(measurement_vector, t)
+
+        if measurement_vector is not None:
+            self.estimator.correction(measurement_vector, t)
 
     def control_vector(self,
                        t,
-                       X_bar,
-                       U_bar,
                        max_iteration,
                        alpha,
                        del_z_tolerance):
         
         optimal_u, X_bar, U_bar = self.controller.control_vector(t,
                                                                  self.estimator.x,
-                                                                 X_bar,
-                                                                 U_bar,
                                                                  max_iteration,
                                                                  alpha,
                                                                  del_z_tolerance)
